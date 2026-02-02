caa6b4174bd2e3e2ecd60e773e43dc41236da7eb
hk99150 <117989893+hk99150@users.noreply.github.com>
2026-02-02 13:05:09 +0530

    domain whitelist

--- mini_lms/settings.py
+++ mini_lms/settings.py
@@ -16,9 +16,9 @@ else:
 
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool('DEBUG', default=True)
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1', "https://instalearnnewbackend-114109844441.asia-south1.run.app/"])
 
-CSRF_TRUSTED_ORIGINS = ["https://devlmsbackend-254015706580.asia-south1.run.app","https://prodlmsbackend-254015706580.asia-south1.run.app"]
+CSRF_TRUSTED_ORIGINS = ["https://instalearnnewbackend-114109844441.asia-south1.run.app/"]
 
 # Application definition
 
