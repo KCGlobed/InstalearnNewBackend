3381d589e77ea527d9edddcac5fad9bcc8e841aa
hk99150 <117989893+hk99150@users.noreply.github.com>
2026-03-05 11:48:01 +0530

    changes

--- adminpanel/course_app/urls.py
+++ adminpanel/course_app/urls.py
@@ -11,14 +11,26 @@ urlpatterns = [
     path('admin-create-new-category/<int:id>', views.create_category, name='admin-create-category'),
     path('admin-create-new-subcategory', views.create_subcategory, name='admin-create-subcategory'),
     path('admin-create-new-subcategory/<int:id>', views.create_subcategory, name='admin-create-subcategory'),
+    path('admin-category-update-status', views.update_category_status, name='admin-update-category-status'),
 
     path('admin-manage-tags', views.manage_tags, name='admin-manage-tags'),
     path('admin-get-tags-listing', views.get_tags_listing, name='admin-get-tags-listing'),
     path('admin-delete-tags/<int:pk>', views.delete_tags, name='admin-delete-tags'),
     path('admin-create-new-tags', views.create_tags, name='admin-create-tags'),
     path('admin-create-new-tags/<int:id>', views.create_tags, name='admin-create-tags'),
+    path('admin-tags-update-status', views.update_tags_status, name='admin-update-tags-status'),
 
     path('admin-manage-ebooks', views.manage_ebooks, name='admin-manage-ebooks'),
     path('admin-get-ebooks-listing', views.get_ebooks_listing, name='admin-get-ebooks-listing'),
+    path('admin-create-new-ebooks', views.create_ebooks, name='admin-create-ebooks'),
+    path('admin-create-new-ebooks/<int:id>', views.create_ebooks, name='admin-create-ebooks'),
+    path('admin-delete-ebooks/<int:pk>', views.delete_ebooks, name='admin-delete-ebooks'),
+    path('admin-ebook-update-status', views.update_ebooks_status, name='admin-update-ebook-status'),
+
+    path('admin-manage-videos', views.manage_videos, name='admin-manage-videos'),
+    path('admin-get-videos-listing', views.get_videos_listing, name='admin-get-videos-listing'),
+    path('admin-create-new-videos', views.create_videos, name='admin-create-videos'),
+    path('admin-get-video-signed-url/', views.get_signed_url, name='admin-get-signed-url'),
+    path('admin-create-new-videos/<int:id>', views.create_videos, name='admin-create-videos'),
 
 ]
--- adminpanel/course_app/views.py
+++ adminpanel/course_app/views.py
@@ -1,23 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from rolepermissions import roles
-from rolepermissions.permissions import available_perm_status
 from mini_lms.roles import *
-from django.contrib.auth import authenticate, login
-from rolepermissions.checkers import has_role
 from users.models import *
 from courses.models import *
 from mini_lms.utils import *
-from django.contrib.auth.models import update_last_login
-from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 from django.contrib.auth.decorators import login_required
-from django.conf import settings
-from django.core.mail import send_mail
-from django.template import loader
-from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
-from django.contrib.auth.tokens import PasswordResetTokenGenerator 
 from django.utils.encoding import smart_str, force_bytes
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+import os
+from django.conf import settings
+import json
+from google.cloud import storage
+from google.oauth2 import service_account
+import calendar
+import time
 
 
 @login_required(login_url='/')
@@ -139,7 +135,6 @@ def get_subcategory_listing(request):
 
 @login_required(login_url='/')
 def manage_subcategory(request):
-    
     return render(request, 'subcategory.html', locals())
 
 
@@ -184,6 +179,32 @@ def delete_category(request, pk):
     except Categories.DoesNotExist:
         return JsonResponse({'status': 'error', 'message': 'Record not found'}, status=404)
     
+
+@require_POST
+def update_category_status(request):
+    category_id = request.POST.get('id')
+    new_status = request.POST.get('status') == 'true'
+
+    try:
+        # Fetch the object
+        category = Categories.objects.get(id=category_id)
+        category.status = new_status
+        category.save()
+
+        return JsonResponse({
+            'status': 'success',
+            'message': f'Category is now {"Active" if new_status else "Inactive"}.'
+        })
+    except Categories.DoesNotExist:
+        return JsonResponse({
+            'status': 'error',
+            'message': 'Category not found.'
+        }, status=404)
+    except Exception as e:
+        return JsonResponse({
+            'status': 'error',
+            'message': str(e)
+        }, status=500)
     
 
 @login_required(login_url='/')
@@ -324,6 +345,32 @@ def delete_tags(request, pk):
         return JsonResponse({'status': 'error', 'message': 'Record not found'}, status=404)
     
 
+@require_POST
+def update_tags_status(request):
+    category_id = request.POST.get('id')
+    new_status = request.POST.get('status') == 'true'
+
+    try:
+        # Fetch the object
+        category = Tags.objects.get(id=category_id)
+        category.status = new_status
+        category.save()
+
+        return JsonResponse({
+            'status': 'success',
+            'message': f'Tag is now {"Active" if new_status else "Inactive"}.'
+        })
+    except Tags.DoesNotExist:
+        return JsonResponse({
+            'status': 'error',
+            'message': 'Tag not found.'
+        }, status=404)
+    except Exception as e:
+        return JsonResponse({
+            'status': 'error',
+            'message': str(e)
+        }, status=500)
+    
 
 @login_required(login_url='/')
 def manage_ebooks(request):
@@ -383,3 +430,210 @@ def get_ebooks_listing(request):
         "recordsFiltered": total_records, # Update this if you add search functionality
         "data": data,
     })
+
+
+@login_required(login_url='/')
+def create_ebooks(request, id=None):
+    if id is not None:
+        ebook_detail = ChapterBooks.objects.filter(id = id).first()
+
+    if request.POST:
+        request.session['data'] = request.POST
+        if request.POST.get('id') != "":
+            tag = ChapterBooks.objects.filter(id=request.POST.get('id')).first()
+            if request.FILES.get('book_file') !="":
+                tag.book_file = request.FILES.get('book_file')
+            tag.name = request.POST.get('name')
+            tag.description = request.POST.get('description')
+            tag.save()
+            return JsonResponse({
+                "status": "success",
+                "message": "eBook updated successfully"
+            })
+        
+        else:
+            tag = ChapterBooks(
+                name = request.POST.get('name'),
+                description = request.POST.get('description'),
+                book_file = request.FILES.get('book_file'),
+                status = True
+            )
+            tag.save()
+
+            return JsonResponse({
+                "status": "success",
+                "message": "eBook created successfully"
+            })
+    
+    return render(request, 'create_ebook.html', locals())
+
+
+@require_POST
+def delete_ebooks(request, pk):
+    try:
+        category = ChapterBooks.objects.get(pk=pk)
+        category.delete()
+        return JsonResponse({'status': 'success', 'message': 'Deleted successfully'})
+    except ChapterBooks.DoesNotExist:
+        return JsonResponse({'status': 'error', 'message': 'Record not found'}, status=404)
+
+
+@require_POST
+def update_ebooks_status(request):
+    category_id = request.POST.get('id')
+    new_status = request.POST.get('status') == 'true'
+
+    try:
+        # Fetch the object
+        category = ChapterBooks.objects.get(id=category_id)
+        category.status = new_status
+        category.save()
+
+        return JsonResponse({
+            'status': 'success',
+            'message': f'Ebook is now {"Active" if new_status else "Inactive"}.'
+        })
+    except ChapterBooks.DoesNotExist:
+        return JsonResponse({
+            'status': 'error',
+            'message': 'Ebook not found.'
+        }, status=404)
+    except Exception as e:
+        return JsonResponse({
+            'status': 'error',
+            'message': str(e)
+        }, status=500)
+    
+
+
+@login_required(login_url='/')
+def manage_videos(request):
+    return render(request, 'videos.html', locals())
+
+
+@login_required(login_url='/')
+def get_videos_listing(request):
+    draw = int(request.GET.get('draw', 1))
+    start = int(request.GET.get('start', 0))
+    length = int(request.GET.get('length', 10))
+    
+    column_mapping = {
+        '0': 'id',
+        '1': 'name',
+        '2': 'created_at', 
+        '3': 'status',
+    }
+    
+    order_index = request.GET.get('order[0][column]', '0')
+    order_dir = request.GET.get('order[0][dir]', 'desc')
+    
+    sort_field = column_mapping.get(order_index, 'id')
+    
+    if order_dir == 'desc':
+        sort_field = '-' + sort_field
+
+    queryset = Videos.objects.all()
+
+    search_value = request.GET.get('search[value]', None)
+    if search_value:
+        # Filter by name or description
+        queryset = queryset.filter(
+            name__icontains=search_value
+        ) | queryset.filter(
+            description__icontains=search_value
+        )
+
+
+    queryset = queryset.order_by(sort_field)
+
+    total_records = queryset.count()
+    data_slice = queryset[start:start + length]
+
+    data = []
+    for obj in data_slice:
+        data.append({
+            "id": obj.id,
+            "name": obj.name or "N/A",
+            "date": obj.created_at.strftime("%Y-%m-%d, %I:%M%p"),
+            "status": obj.status, # Sending as boolean to handle in JS
+        })
+
+    return JsonResponse({
+        "draw": draw,
+        "recordsTotal": total_records,
+        "recordsFiltered": total_records, # Update this if you add search functionality
+        "data": data,
+    })
+
+
+def get_signed_url(request):
+    if request.method == "POST":
+        
+        info = json.loads(os.environ.get("GOOGLE_CREDENTIALS_JSON"))
+        credentials = service_account.Credentials.from_service_account_info(info)
+
+        storage_client = storage.Client(credentials=credentials, project=credentials.project_id)
+
+        bucket_name = settings.GS_BUCKET_NAME
+        bucket = storage_client.bucket(bucket_name)
+
+        current_GMT = time.gmtime()
+        ts = calendar.timegm(current_GMT)
+        unique_file_name = f"{ts}.mp4"
+        
+        path = "mini_lms/videos"
+        blob_path = f"media/{path}/{unique_file_name}"
+        blob = bucket.blob(blob_path)
+
+        content_type = 'video/mp4'
+
+        try:
+            # Generate the signed URL
+            signed_url = blob.generate_signed_url(
+                version='v4',
+                expiration=3600, # 1 hour is usually plenty for an upload start
+                method='PUT',
+                content_type=content_type
+            )
+
+            # This is the final URL where the file will live
+            public_url = f"https://storage.googleapis.com/{bucket_name}/{blob_path}"
+
+            return JsonResponse({
+                'signed_url': signed_url,
+                'public_url': public_url,
+                "video_file_url": path+"/"+unique_file_name
+            })
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+        
+
+
+@login_required(login_url='/')
+def create_videos(request, id=None):
+    if id is not None:
+        video_detail = Videos.objects.filter(id = id).first()
+
+    if request.method == "POST":
+        video_id = request.POST.get('id')
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        video_url = request.POST.get('video_url') # The public_url from the frontend
+
+        if video_id:
+            video = Videos.objects.get(id=video_id)
+            video.name = name
+            video.description = description
+            if video_url:
+                video.video_file = video_url
+            video.save()
+        else:
+            Videos.objects.create(
+                name=name,
+                description=description,
+                video_file=video_url
+            )
+            
+        return JsonResponse({'message': 'Video saved successfully!'})
+    
+    return render(request, 'create_videos.html', locals())
