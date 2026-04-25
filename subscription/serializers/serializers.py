2bf10c90a36aa70665ed03134aa9cd443e31e517
hk99150 <117989893+hk99150@users.noreply.github.com>
2026-04-25 18:25:56 +0530

    Update serializers.py

--- subscription/serializers/serializers.py
+++ subscription/serializers/serializers.py
@@ -52,7 +52,7 @@ class AddtoCartSerializer(serializers.ModelSerializer) :
 class CourseInfoListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = ['name',"image","price"]
+        fields = ["id",'name',"image","price"]
 
 
 class CartSerializer(serializers.ModelSerializer):
