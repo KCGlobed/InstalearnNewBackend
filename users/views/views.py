7be2a91643c70fc629fd4175d9166f627ce26c3a
hk99150 <117989893+hk99150@users.noreply.github.com>
2026-05-01 09:46:07 +0530

    Update views.py

--- users/views/views.py
+++ users/views/views.py
@@ -102,7 +102,7 @@ class UserLoginView(APIView):
                 user.locked_until = None
                 user.save()
 
-                return success_response(message="Login Success", data={'token': token, 'user_role': serializer.data.get('role'), "user_id":user.id}, status_code=status.HTTP_200_OK)
+                return success_response(message="Login Success", data={'token': token, 'user_role': serializer.data.get('role'), "user_id":user.id,"email":user.email,"first_name":user.first_name,"last_name":user.last_name,"phone":user.phone1}, status_code=status.HTTP_200_OK)
             else:
                 return error_response(message="failed", data = {}, status_code=status.HTTP_400_BAD_REQUEST)
         
