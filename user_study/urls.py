499c2b66c489574ad5e780f956167f29e1f19c8d
hk99150 <117989893+hk99150@users.noreply.github.com>
2026-07-20 11:44:56 +0530

    api changes

--- user_study/urls.py
+++ user_study/urls.py
@@ -61,7 +61,7 @@ urlpatterns = [
     path('get-user-study-progress/', GetUserStudyProgressView.as_view(), name="get-user-study-progress"),
     path('get-user-courses-progress/', GetUserCoursesProgressView.as_view(), name="get-user-courses-progress"),
     path('reshare-user-login-detail/', ReshareUserLoginDetailView.as_view(), name="reshare-user-login-detail"),
-
+    path('get-corporate-students-list/', GetCorporateStudentsListView.as_view(), name="get-corporate-students-list"),
     path('assign-single-course-to-students/', AssignSingleCourseAccessView.as_view(), name="assign-single-course-to-students"),
 
 ]
--- user_study/views.py
+++ user_study/views.py
@@ -1231,3 +1231,16 @@ class AssignSingleCourseAccessView(APIView):
             user= serializer.save()
             return success_response(message="Course Access Shared Successfully", data=StudentListingSerializer(user).data, status_code=status.HTTP_200_OK)
         return error_response(message="failed", data = serializer.errors, status_code=status.HTTP_400_BAD_REQUEST)
+    
+
+
+class GetCorporateStudentsListView(APIView):
+    renderer_classes = [UserStudyRenderer]
+    permission_classes = [IsAuthenticated, 
+                          RoleOrPermissionCheck.for_roles(
+                            [CorporateAdmin]
+                        )]
+    def get(self, request, format=None):
+        users_list = User.objects.filter(corporate = request.user)
+        serializer = StudentListingSerializer(users_list, many=True)
+        return success_response(message="Success", data=serializer.data, status_code=status.HTTP_200_OK)
